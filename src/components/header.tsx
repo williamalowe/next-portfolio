ce9867a699058f1aa810d335c3d5c619807986d6
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-08 15:31:27 +1000

    styles: adjust theme-switcher styles

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
     <header>
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-zinc border-opacity-40 
-        bg-zinc-50 bg-opacity-80 shadow-lg shadow-zinc/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] 
+        bg-zinc-50 bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] 
         sm:w-[36rem] sm:rounded-md dark:bg-zinc-900 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
--- src/components/theme-switch.tsx
+++ src/components/theme-switch.tsx
@@ -7,7 +7,12 @@ export default function ThemeSwitch() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme} className="fixed bottom-4 right-4 w-8 h-8 bg-zinc-900/10 flex items-center justify-center rounded-md dark:bg-zinc-50/10 sm:w-12 sm:h-12 sm:text-xl">
+    <button
+      onClick={toggleTheme}
+      className="fixed bottom-4 right-4 w-8 h-8 bg-zinc-50 bg-opacity-80 shadow-lg shadow-black/[0.03] 
+      border border-black/20 border-opacity-40 backdrop-blur-[0.5rem] flex items-center justify-center 
+      rounded-md dark:bg-zinc-900 dark:border-black/40 dark:bg-opacity-75 sm:w-12 sm:h-12 sm:text-xl"
+    >
       {theme === "light" ? <BsSun /> : <BsMoon />}
     </button>
   );
