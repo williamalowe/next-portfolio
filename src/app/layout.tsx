386de529e9b9e010d689adfb57e3f4e70f1dcb38
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-07-08 14:20:03 +1000

    refactor: remove project files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { M_PLUS_1_Code } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
 
 const font = M_PLUS_1_Code({ subsets: ["latin"] });
 
@@ -20,9 +19,6 @@ export default function RootLayout({
       <body
         className={`${font.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}
       >
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-        <Header />
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Intro from "@/components/intro";
-import SectionDivider from "@/components/section-divider";
-
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <Intro />
-      <SectionDivider />
+      hello world
     </main>
   );
 }
